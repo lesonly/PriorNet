f31c799cafe158d89713cd7db561880dc3fe5f59
lesonly <58156801+lesonly@users.noreply.github.com>
2022-08-20 12:30:23 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,6 @@ from torch.utils.data import DataLoader
 from tensorboardX import SummaryWriter
 from data4 import dataset
 from net_Prior import PirorNet
-#from ynet import GCPANet
 import logging as logger
 from lib4.data_prefetcher import DataPrefetcher
 from lib4.lr_finder import LRFinder
@@ -20,7 +19,6 @@ import matplotlib.pyplot as plt
 import pytorch_iou2
 import pytorch_iou
 import pytorch_dhm
-#from edge_loss import EdgeLoss2d
 from utils.generator import make_one_hot
 from utils.loss import AffinityLoss
 #import torch.distributed as dist
@@ -34,16 +32,6 @@ import pytorch_iou
 bce_loss = nn.BCELoss(size_average=True)
 ssim_loss = pytorch_ssim.SSIM(window_size=11,size_average=True)
 iou_loss = pytorch_iou.IOU(size_average=True)
-
-def bce_ssim_loss(pred,target):
-
-	bce_out = F.binary_cross_entropy_with_logits(pred,target)
-	#ssim_out = 1 - ssim_loss(F.sigmoid(pred),target)
-	iou_out = iou_loss(F.sigmoid(pred),target)
-
-	loss = torch.mean(bce_out  + iou_out)# + ssim_out)
-
-	return loss
 
 
 # os.environ['MASTER_ADDR'] = '127.0.0.1'
@@ -79,16 +67,6 @@ def get_triangle_lr(base_lr, max_lr, total_steps, cur, ratio=1., \
 
     return lr, momentum
 
-def structure_loss(pred, mask):
-    weit  = 1+5*torch.abs(F.avg_pool2d(mask, kernel_size=31, stride=1, padding=15)-mask)
-    wbce  = F.binary_cross_entropy_with_logits(pred, mask, reduce='none')
-    wbce  = (weit*wbce).sum(dim=(2,3))/weit.sum(dim=(2,3))
-
-    pred  = torch.sigmoid(pred)
-    inter = ((pred*mask)*weit).sum(dim=(2,3))
-    union = ((pred+mask)*weit).sum(dim=(2,3))
-    wiou  = 1-(inter+1)/(union-inter+1)
-    return (wbce+wiou).mean()
 
 def box_loss(pred,mask, di,er):
     weit1  = 1+5*torch.abs(F.avg_pool2d(di, kernel_size=31, stride=1, padding=15)-di)
@@ -120,8 +98,6 @@ FIND_LR = False #True#
 # ------- 1. define loss function --------
 
 
-
-
 def train(Dataset, Network):
     
 
@@ -145,9 +121,6 @@ def train(Dataset, Network):
 
     cfg    = Dataset.Config(datapath='./data/DUTS', savepath=SAVE_PATH, mode='train', batch=args.batch_size, lr=0.05, momen=0.9, decay=5e-4, epoch=36)
     data   = Dataset.Data(cfg)
-    #loader = DataLoader(data, batch_size=cfg.batch, shuffle=True,drop_last=True,num_workers=4)
-    #train_sampler = torch.utils.data.distributed.DistributedSampler(data)
-    #loader = DataLoader(data, batch_size=cfg.batch, num_workers=4, pin_memory=True, drop_last=True,sampler=train_sampler)
     loader = DataLoader(data, batch_size=cfg.batch, num_workers=4, pin_memory=True, drop_last=True,sampler=DistributedSampler(data,shuffle=True))
 
     prefetcher = DataPrefetcher(loader)
@@ -157,8 +130,6 @@ def train(Dataset, Network):
     #net = torch.nn.SyncBatchNorm.convert_sync_batchnorm(net)
   
     net.train(True)
-    #net=nn.DataParallel(net,device_ids=[0,1,2,3]) 
-    # net.cuda()
     net.to(device)
     net = torch.nn.parallel.DistributedDataParallel(net,device_ids=[local_rank],output_device=local_rank,find_unused_parameters=True)      
 
@@ -173,20 +144,10 @@ def train(Dataset, Network):
         else:
             head.append(param)
     optimizer   = torch.optim.SGD([{'params':base},{'params':edger},{'params':head}], lr=cfg.lr, momentum=cfg.momen, weight_decay=cfg.decay, nesterov=True)
-
-   
-
     db_size = len(loader)
-    if FIND_LR:
-        os.environ['DISPLAY'] = '138.138.0.129:0.0'
-        lr_finder = LRFinder(net, optimizer, criterion=None)
-        lr_finder.range_test(loader,start_lr = 1e-6, end_lr=50, num_iter=330, step_mode="exp")
-        plt.ion()
-        lr_finder.plot()
-        import pdb; pdb.set_trace()
+
     # #training
 
-    #for epoch2 in range(0,456):
 
     global_step = 0
     p=None
@@ -218,28 +179,17 @@ def train(Dataset, Network):
             loss3  = box_loss(out3,mask, di,er)
             loss4  = box_loss(out4,mask, di,er)
             loss5  = box_loss(out5,mask, di,er)    
-            loss   = loss2*1 + loss3*0.8 + loss4*0.6 + loss5*0.4
-
-            
-            #p.detach_()  
-            # dilated=thickedge_region + mask
-            # dilated[dilated>1]=1
-            # dilated=(gt_thickedge_region | gt_mask).float()
-            #loss_coarse=F.binary_cross_entropy_with_logits(out2_coarse_a, dilated)+iou_loss(F.sigmoid(out2_coarse_a), dilated)#).mean()
-            #loss=loss_fine+loss_coarse#+loss_hm
+            loss   = loss2*1 + loss3*0.8 + loss4*0.6 + loss5*0.4           
+     
     
             #reduce_loss = all_reduce_tensor(loss, world_size=4)
-            optimizer.zero_grad()
-            #optimizer2.zero_grad()
-          
-            loss.backward()                          
-            
-            #optimizer2.step()
+            optimizer.zero_grad()       
+            loss.backward()                        
             optimizer.step()
             
         
             if batch_idx % 10 == 0:               
-                #msg = '%s | step:%d/%d/%d | lr=%.6f | loss=%.6f | loss_fore=%.6f | loss_back=%.6f | loss_edge=%.6f'%(datetime.datetime.now(),  global_step, epoch+1, cfg.epoch, optimizer.param_groups[0]['lr'], loss.item(), loss_fore.item(), loss_back.item(),10*loss_edge.item())
+                
                 msg = '%s | step:%d/%d/%d | lr=%.6f | loss=%.6f | loss2=%.6f | loss3=%.6f | loss4=%.6f | loss5=%.6f | loss=%.6f'% (
                     datetime.datetime.now(), global_step, epoch + 1, cfg.epoch, optimizer.param_groups[0]['lr'],
                     loss.item(), loss2.item(), loss3.item(), loss4.item(), loss5.item(),loss.item())
@@ -249,9 +199,9 @@ def train(Dataset, Network):
             image, mask,thickedge_region,di,er= prefetcher.next()
         
         #scheduler.step()
-        if epoch>31:#cfg.epoch/9*8:
-            #torch.save(net.module.state_dict(), cfg.savepath+'/model64_32_net17f_three_short_1220_2'+str(epoch+1))
-            torch.save(net.module.state_dict(), cfg.savepath+'/7.4.w1'+str(epoch+1))#
+        if epoch==36:
+           
+            torch.save(net.module.state_dict(), cfg.savepath+'/Ours'+str(epoch+1))#
 
 
 
